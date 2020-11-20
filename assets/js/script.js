aea47b6c76ac55a9e6d41785802a5a122042be3e
CassBut <clhurlbut@gmail.com>
2020-11-19 20:17:47 -0800

    added saving functionality to the input

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,21 @@
 
 // Define Variables 
 
+
 // Create the opening page which has the cookie (together, lol) and the input
 
 // Get the input from the user and save it to local storage 
+$("#searchBtn").click(function(){
+    var moodInput = $("#inputMood").val()
+    console.log(moodInput)
+    localStorage.setItem("mood", moodInput)
+}
 
+);
 // Take the input from user and run a search with Quotes.REST API to return quote with limited integers and SFW rating
 
 // Take input from user and run a search in GIPHY API to return GIF with SFW rating
+
 
 // Log the 4 previous fortunes below - use logic to make it so when you click the old fortune it takes the string from local storage and 
 // runs the function to retrieve the quote and gif for it. Might not be the same one each time, but that is ok 
