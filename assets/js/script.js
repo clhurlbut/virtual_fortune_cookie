26bca54eeb7a929d667dfcc7dff087e0a8e90cd2
CassBut <clhurlbut@gmail.com>
2020-11-19 16:13:31 -0800

    tried animating the cookie, not working

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+
+$('#searchBtn').onClick(function(){
+    $('#cookieLeft').addClass('animate_animated', 'animate_rotateOutUpLeft');
+    $('#cookieRight').addClass('animate_animated', 'animate_rotateOutUpRight');
+});
