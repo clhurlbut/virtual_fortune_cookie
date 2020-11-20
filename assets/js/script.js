fccfd75aefb35f043b96be1cb75684998c1896f7
CassBut <clhurlbut@gmail.com>
2020-11-19 21:38:33 -0800

    added giphy API functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,20 +7,55 @@ var moodInput = $("#inputMood").val()
 
 let results = []
 // Get the input from the user and save it to local storage 
-$("#searchBtn").click(function () {
+$("#searchBtn").click(function() {
     let searchResult = {}
     searchResult.input
     searchResult.result
     searchResult.input = $("#inputMood").val()
-
     // fetch results from giphy based on term 
+    getGiphyData(searchResult.input)
+    .then(giphyData => {
+        searchResult.result = giphyData
+        console.log(giphyData)
+        storeMoodResult(searchResult);
+        renderGiphyData(searchResult.result);
+    })
+    
     // fetch results from quotes based on terms
-    // store results to storage 
-    storeMoodResult(searchResult);
+    
 
 
 }
 );
+function renderGiphyData(data) {
+    let giphyContainer = $("#giphyCont")
+      giphyContainer.html(
+      `
+      <img src="${data[0].images.fixed_height.url}"/>
+      `    
+      );
+
+      // string interpelation above
+      // giphyContainer.innerHTML = ""
+      // var gifImg = document.createElement('img');
+      // gifImg.setAttribute('src', data[0].images.fixed_height.url);
+      
+      // giphyContainer.appendChild(gifImg);
+
+}
+function getGiphyData(inputMood){
+    return new Promise((resolve, reject) =>{
+        fetch("https://api.giphy.com/v1/gifs/search?q=" + inputMood + "&api_key=m1Xfg0VXns59b9T57eo1ghhh0KJYrA1c&limit=1")
+        .then(function(response) {
+          return response.json();
+        })
+        .then (result => resolve(result.data))
+        .catch(err => reject(err))
+    });
+    
+     
+    }
+
 
 function storeMoodResult(result) {
     results.push(result)
