2195e1409c5e606023c3d8078c806286250df28e
CassBut <clhurlbut@gmail.com>
2020-12-02 22:16:56 -0800

    working with code from kassim

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ var moodInput = $("#inputMood").val()
 var cookieAnimateRight = $("#cookieRight");
 var cookieAnimateLeft = $("#cookieLeft");
 
-let results = []
+let results = JSON.parse(localStorage.getItem("mood")) || []
 
 
 
@@ -162,8 +162,22 @@ fetch("https://type.fit/api/quotes")
         renderQuoteData(data)
         // console.log(data)
     })
-// function to store the results 
+
 function storeMoodResult(input) {
+    if (results.length === 4) {
+        results.unshift()
+    }
     results.push(input)
     localStorage.setItem("mood", JSON.stringify(results))
+    renderMoodStorage()
+}
+
+
+// function for refresh/loaded get mood from local storage
+function renderMoodStorage() {
+    for (let i = 0; i < results.length; i++) {
+        var quoteStorage = $("<p>").text(results[i])
+        $(`#${i}`).append(quoteStorage)
+
+    }
 }
